ddae7d4c2be71336ce26f361517f93c58074f94c
CodeMongerX <37663008+CodeMongerX@users.noreply.github.com>
2019-04-11 22:05:44 +0530

    String multiget and set
    Set using a dict and get returns a list

--- stringExamples.py
+++ stringExamples.py
@@ -6,10 +6,16 @@ r = redis.Redis(host='127.0.0.1',
                 encoding=u'utf-8',
                 decode_responses=True)
 
-# Multi-SET of keys
-r.mset('first', "First Key Value", 'second', "Second key Value")
+# Multi-SET of keys: use dict for all these
+r.mset({'first': 'FirstKeyValue', 'second': 'SecondkeyValue'})
 
 # Multi-Get
-response = r.mget('first', 'second')
+response = r.mget(*('first', 'second'))
+print("MSET & MGET: ", end="")
 print(response)
 
+
+# set so that it expires after 10 seconds
+r.setex(name="ExpireKey", time=10, value="Boom!!!")
+
+r.set(name="key", value="hello", ex=5, px=None, nx=True, xx=False)
